0d9172273753e0e24e5883bdabc01ee95f1168f6
AdelGann <adelchaficgc@gmail.com>
2024-12-29 13:32:51 -0400

    feat: add toaster, add custom components

---
+++ src/components/custom/InputField/InputField.tsx
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+}
+
+export function InputField({ label, className, ...props }: InputFieldProps) {
+    return (
+			<div>
+				<Label>{label}</Label>
+				<Input className={`${className} hover:bg-gray-100`} {...props} />
+			</div>
+		);
+}
---
+++ src/components/custom/SelectField/SelectField.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label"; // Importa el componente Label
+
+interface SelectFieldProps {
+	options: { value: string; name: string }[] | [];
+	name?: string;
+	value?: string;
+	onChange?: (value: string) => void;
+	placeholder?: string;
+	disabled?: boolean;
+	label?: string;
+	className?: string;
+}
+
+export const SelectField: React.FC<SelectFieldProps> = ({
+	options,
+	placeholder,
+	className,
+	disabled = false,
+	name,
+	value,
+	onChange,
+	label,
+}) => {
+	return (
+		<div>
+			{label && <Label htmlFor={name}>{label}</Label>}
+			<Select disabled={disabled} value={value} onValueChange={onChange} name={name}>
+				<SelectTrigger className={`${className}  hover:bg-gray-100`} disabled={disabled}>
+					<SelectValue placeholder={placeholder} />
+				</SelectTrigger>
+				<SelectContent>
+					{options.map((option) => (
+						<SelectItem key={option.value} value={option.value}>
+							{option.name}
+						</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+		</div>
+	);
+};
