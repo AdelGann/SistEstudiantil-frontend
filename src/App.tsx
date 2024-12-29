a9a1bfcb655ba62ecde22ddc384f9c4bb263eb6d
AdelGann <adelchaficgc@gmail.com>
2024-12-29 13:33:10 -0400

    feat: toaster configurated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { AppRoutes } from "./routes/App.routes";
-
+import { Toaster } from "@/components/ui/toaster";
 function App() {
 	return (
 		<>
 			<AppRoutes />
+			<Toaster />
 		</>
 	);
 }
